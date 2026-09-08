5d4c5cc7edff7882f6f9b6fa494724b38e28cefc
Himasha Sawani <himashasawani@gmail.com>
2026-09-08 08:20:16 +0530

    implement MVVM architecture with view models, services, and RelayCommand support

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-using System.Text;
+using RetailFlow.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RetailFlow;
 
@@ -16,8 +8,14 @@ namespace RetailFlow;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private readonly MainViewModel _viewModel;
+
     public MainWindow()
     {
         InitializeComponent();
+        _viewModel = new MainViewModel();
+        DataContext = _viewModel;
+
+        Loaded += async (_, _) => await _viewModel.InitializeAsync();
     }
 }
