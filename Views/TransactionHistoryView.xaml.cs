15d7a9554718458380d185756b6ee880426d4cce
Himasha Sawani <himashasawani@gmail.com>
2026-09-08 08:31:22 +0530

    add SaleReceipt model and BarHeightConverter helper class

---
+++ Views/TransactionHistoryView.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows.Controls;
+
+namespace RetailFlow.Views;
+
+/// <summary>
+/// Interaction logic for TransactionHistoryView.xaml
+/// </summary>
+public partial class TransactionHistoryView : UserControl
+{
+    public TransactionHistoryView()
+    {
+        InitializeComponent();
+    }
+}
